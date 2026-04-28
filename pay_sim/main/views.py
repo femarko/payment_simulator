b04736fd0999e74ee37b445b8508e1ddc8bf7ca2
femarko <fenstermark7@gmail.com>
2026-04-28 15:22:56 +0200

    feat(views): add index, check_status (rendering) and PaymentViewSet

--- pay_sim/main/views.py
+++ pay_sim/main/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from main.serializers import PaymentSerializer
 
-# Create your views here.
+from main.models import Payment
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def check_status(request) -> HttpResponse:
+    id = request.GET.get('id')
+    payment = Payment.objects.get(id=id)
+    status = payment.status
+    return render(request, 'check_status.html', {'status': status})
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+
